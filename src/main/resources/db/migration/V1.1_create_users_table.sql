fde9b264bbfa1ceba0187cb3ecc047c8d9bcae69
Mingqian Liu <630616898@qq.com>
2020-01-31 11:16:11 -0500

    database setting

--- src/main/resources/db/migration/V1.1_create_users_table.sql
+++
@@ -1,8 +0,0 @@
-create table "users"(
-	user_id    BIGSERIAL    NOT NULL,
-	user_name  VARCHAR(255) NOT NULL UNIQUE,
-	password   VARCHAR(255) NOT NULL,
-	email      VARCHAR(255) NOT NULL UNIQUE,
-	phone      VARCHAR(255) NOT NULL UNIQUE,
-	PRIMARY KEY(user_id),
-);
