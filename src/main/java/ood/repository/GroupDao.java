6a60de70b0c0752053ce4bd9199419cd651603e4
zhibosheng <kage123zhibosheng@gmail.com>
2020-02-02 14:20:47 -0500

    add service layer

--- src/main/java/ood/repository/GroupDao.java
+++ src/main/java/ood/repository/GroupDao.java
@@ -1,9 +1,13 @@
 package ood.repository;
 
+import ood.model.Event;
 import ood.model.Group;
+
+import java.util.List;
 
 public interface GroupDao {
     Group save(Group group);
     Group update(Group group);
     boolean delete(Group group);
+    List<Event> getHistory(Group group);
 }
