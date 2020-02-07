d43854b20ce290fcd50fd03490d37deec14b47ee
zhibosheng <kage123zhibosheng@gmail.com>
2020-02-07 17:59:57 -0500

    rating api

--- src/main/java/ood/service/MovieAPIService.java
+++ src/main/java/ood/service/MovieAPIService.java
@@ -1,6 +1,7 @@
 package ood.service;
 
 
+import com.amazonaws.services.dynamodbv2.xspec.S;
 import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.Unirest;
 import com.squareup.okhttp.OkHttpClient;
@@ -12,6 +13,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class MovieAPIService {
@@ -35,26 +40,23 @@ public class MovieAPIService {
 //        }
 //    }
 
-    public URL getMovie(String ttId){
+    public URL getTrailerUrl(String ttId){
         URL url = null;
         try{
-            HttpResponse<String> response = Unirest.get("https://imdb8.p.rapidapi.com/title/get-videos?limit=25&region=US&tconst="+ttId)
+            HttpResponse<String> response = Unirest.get("https://imdb8.p.rapidapi.com/title/get-videos?limit=1&region=US&tconst="+ttId)
                     .header("x-rapidapi-host", "imdb8.p.rapidapi.com")
                     .header("x-rapidapi-key", "21fb4432b2msh936d28e4bc4b053p137a56jsn3fa00555b0d1")
                     .asString();
-            System.out.println(response.getBody());
+//            System.out.println(response.getBody());
             JSONObject jsonObject = new JSONObject(response.getBody());
-
-            JSONObject resource = new JSONObject(jsonObject.get("resource").toString());
-
-
+            JSONObject resource = (JSONObject) jsonObject.get("resource");
             JSONArray array = (JSONArray) resource.get("videos");
 //            System.out.println(array.get(0));
-            JSONObject video = new JSONObject(array.get(0).toString());
+            JSONObject video = (JSONObject)array.get(0);
             String videoId = video.get("id").toString();
             String[] viArr = videoId.split("/");
             String vi = viArr[2];
-            System.out.println(vi);
+//            System.out.println(vi);
             url = new URL("https://www.imdb.com/video/"+vi);
         } catch(Exception e){
             System.out.println(e.getMessage());
@@ -62,5 +64,50 @@ public class MovieAPIService {
         return url;
     }
 
+    public List<HashMap> getUserReviewsList(String ttId){
+        List<HashMap> userReviewsList = new ArrayList<HashMap>();
+        try{
+            HttpResponse<String> response = Unirest.get("https://imdb8.p.rapidapi.com/title/get-user-reviews?tconst="+ttId)
+                    .header("x-rapidapi-host", "imdb8.p.rapidapi.com")
+                    .header("x-rapidapi-key", "21fb4432b2msh936d28e4bc4b053p137a56jsn3fa00555b0d1")
+                    .asString();
+//            System.out.println(response.getBody());
+            JSONObject jsonObject = new JSONObject(response.getBody());
+            JSONArray array = (JSONArray) jsonObject.get("reviews");
+            for (int i=0; i<array.length();i++){
+                HashMap<String, String> userReview  = new HashMap<>();
+                JSONObject arr = (JSONObject) array.get(i);
+                JSONObject author = (JSONObject) arr.get("author");
+                String displayName = author.get("displayName").toString();
+                String reviewTitle = arr.get("reviewTitle").toString();
+                String reviewText = arr.get("reviewText").toString();
+                String submissionDate = arr.get("submissionDate").toString();
+                userReview.put("displayName",displayName);
+                userReview.put("reviewTitle",reviewTitle);
+                userReview.put("reviewText",reviewText);
+                userReview.put("submissionDate",submissionDate);
+                userReviewsList.add(userReview);
+            }
+        } catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+        return userReviewsList;
+    }
+
+    public String getRating(String ttId){
+        String rating = "";
+        try{
+            HttpResponse<String> response = Unirest.get("https://imdb8.p.rapidapi.com/title/get-ratings?tconst="+ttId)
+                    .header("x-rapidapi-host", "imdb8.p.rapidapi.com")
+                    .header("x-rapidapi-key", "21fb4432b2msh936d28e4bc4b053p137a56jsn3fa00555b0d1")
+                    .asString();
+//            System.out.println(response.getBody());
+            JSONObject jsonObject = new JSONObject(response.getBody());
+            rating = jsonObject.get("rating").toString();
+        } catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+        return rating;
+    }
 
 }
--- src/test/java/ood/service/MovieAPIServiceTest.java
+++ src/test/java/ood/service/MovieAPIServiceTest.java
@@ -9,16 +9,32 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import java.net.URL;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = ApplicationBoot.class)
 public class MovieAPIServiceTest {
     @Autowired
     MovieAPIService movieAPIService;
+    String ttId = "tt0848228";
     @Test
     public void getVideoUrl(){
-        String ttId = "tt0944947";
-        URL url = movieAPIService.getMovie(ttId);
+        URL url = movieAPIService.getTrailerUrl(ttId);
+        System.out.println(url);
         Assert.assertNotNull(url);
+    }
+
+    @Test
+    public void getUserReviewsList(){
+        List<HashMap>  userReviewsList = movieAPIService.getUserReviewsList(ttId);
+        Assert.assertNotNull(userReviewsList);
+    }
+
+    @Test
+    public void getRating(){
+        String rating= movieAPIService.getRating(ttId);
+        Assert.assertNotNull(rating);
     }
 }
