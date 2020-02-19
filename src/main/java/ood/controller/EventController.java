ddb7830510c8fe2cd81cc2a366988422b80477a4
Mingqian Liu <630616898@qq.com>
2020-02-19 14:32:30 -0500

    fix controller problem

--- src/main/java/ood/controller/EventController.java
+++ src/main/java/ood/controller/EventController.java
@@ -8,9 +8,11 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.time.OffsetDateTime;
 
+@RestController
 public class EventController {
     @Autowired
     EventService eventService;
--- src/main/java/ood/controller/VotingController.java
+++ src/main/java/ood/controller/VotingController.java
@@ -7,9 +7,11 @@ import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.time.OffsetDateTime;
 
+@RestController
 public class VotingController {
     @Autowired
     VotingService votingService;
