ea5e7b84191f378a54618d8990a12178dccad33f
Mingqian Liu <630616898@qq.com>
2020-04-02 20:37:37 -0400

    add groupService test

---
+++ src/test/java/ood/service/GroupServiceTest.java
@@ -0,0 +1,43 @@
+package ood.service;
+
+
+import ood.ApplicationBoot;
+import ood.model.Group;
+import ood.model.User;
+import org.junit.After;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = ApplicationBoot.class)
+public class GroupServiceTest {
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private GroupService groupService;
+
+    private Logger logger = LoggerFactory.getLogger(this.getClass());
+
+    private User userRecord1 = new User();
+    private Group groupRecord1 = new Group();
+
+    @Test
+    public void createGroup(){
+        userRecord1.setUserName("Alice");
+        userRecord1.setEmail("12324@qq.com");
+        userRecord1.setPhone("2028538799");
+        userRecord1.setPassword("123456");
+        userService.save(userRecord1);
+
+        groupRecord1.setModerator(userRecord1);
+        groupRecord1.setGroupName("groupA");
+        groupRecord1.setGroupDescription("This is A");
+        groupService.save(groupRecord1);
+    }
+
+}
